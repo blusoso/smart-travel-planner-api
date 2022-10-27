36a755aa7cde9e2a9feb4e40e32d3eaa94a920d3
bluso <fahstjlps@gmail.com>
2022-10-27 20:57:23 +0700

    [feat]: add combined text column to place infomation for create recommendation system

--- app/src/domain/places/information/model.py
+++ app/src/domain/places/information/model.py
@@ -10,14 +10,14 @@ class PlaceInformation(Base):
     __tablename__ = 'place_information'
 
     id = Column(Integer, primary_key=True, index=True)
-    place_id = Column(String, ForeignKey('places.id'))
+    place_id = Column(String(20), ForeignKey('places.id'))
     intro = Column(Text, nullable=True)
     description = Column(Text, nullable=True)
     activities = Column(ARRAY(String), nullable=True)
     facilities = Column(ARRAY(String), nullable=True)
     how_to_travel = Column(Text, nullable=True)
-    how_to_travel = Column(Text, nullable=True)
-    language_code_id = Column(String, ForeignKey('language_codes.id'))
+    combined_text = Column(Text, nullable=True)
+    language_code_id = Column(String(3), ForeignKey('language_codes.id'))
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
